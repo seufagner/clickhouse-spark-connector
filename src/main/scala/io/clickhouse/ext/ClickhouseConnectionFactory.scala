b7782e2b4528eb0d21b3219c339b1974e69f3ce9
Fagner Moura <fagner.moura@edumobi.com.br>
2017-07-18 14:50:50 -0300

    receive db name before to parse jdbc url

--- src/main/scala/io/clickhouse/ext/ClickhouseConnectionFactory.scala
+++ src/main/scala/io/clickhouse/ext/ClickhouseConnectionFactory.scala
@@ -1,19 +1,20 @@
 package io.clickhouse.ext
 
 import java.util.Properties
+
 import ru.yandex.clickhouse.ClickHouseDataSource
 import ru.yandex.clickhouse.settings.ClickHouseProperties
 
-object ClickhouseConnectionFactory extends Serializable{
+object ClickhouseConnectionFactory extends Serializable {
 
   private val dataSources = scala.collection.mutable.Map[(String, Int), ClickHouseDataSource]()
 
-  def get(host: String, port: Int = 8123): ClickHouseDataSource ={
+  def get(host: String, port: Int = 8123, dbName: String = "default"): ClickHouseDataSource = {
     dataSources.get((host, port)) match {
       case Some(ds) =>
         ds
       case None =>
-        val ds = createDatasource(host, port = port)
+        val ds = createDatasource(host, Some(dbName), port = port)
         dataSources += ((host, port) -> ds)
         ds
     }
@@ -21,7 +22,7 @@ object ClickhouseConnectionFactory extends Serializable{
 
   private def createDatasource(host: String, dbO: Option[String] = None, port: Int = 8123) = {
     val props = new Properties()
-    dbO map {db => props.setProperty("database", db)}
+    dbO map { db => props.setProperty("database", db) }
 
     val clickHouseProps = new ClickHouseProperties(props)
     new ClickHouseDataSource(s"jdbc:clickhouse://$host:$port", clickHouseProps)
